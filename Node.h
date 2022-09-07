1b1a2a29a99f1f66b37efb949817c5d6fbf35022
Maxim Huzmiev <max@huzm.ru>
2022-09-07 10:32:39 +0000

    Connect and Disconnect methods for Node

--- Node.h
+++ Node.h
@@ -1,5 +1,7 @@
 #pragma once
 
+enum { LEFT_CHILD, RIGHT_CHILD };
+
 /*****************************************
 Class: Node of binary tree
 ******************************************/
@@ -10,7 +12,9 @@ private:
     Node *parent, *left, *right;
 public:
     Node(int, Node*);
-    void connect_child(Node*);
+    bool connect_child(Node*);
+    Node * disconnect_child(int);
+    
 };
 /*****************************************/
 
@@ -26,7 +30,41 @@ Node::Node(int val, Node * parent = nullptr)
 
 // Other Methods definitions
 
-void Node::connect_child(Node * child)
+/* 
+ Connect child node to this node
+ -> new child node pointer 
+ <- true if success, false if subtree already exists
+*/
+bool Node::connect_child(Node * child)
 {
-
+    if (child->value < this->value) {
+        if (this->left) return false;
+        this->left = child;
+    }
+    else {
+        if (this->right) return false;
+        this->right = child;
+    }
+    return true;
 }
+
+
+/* 
+ Disconnect child node from this node
+ -> what of 2 childs (LEFT_CHILD or RIGHT_CHILD)
+ <- pointer to the disconnected child node
+*/
+Node * Node::disconnect_child(int left_or_right)
+{
+    Node * disconnected = nullptr;
+    if (left_or_right == LEFT_CHILD) {
+        disconnected = this->left;
+        this->left = nullptr;
+    }
+    else {
+        disconnected = this->right;
+        this->right = nullptr;
+    }
+    return disconnected;
+}
+
